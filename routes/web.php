c8d80e6eb1745d01fd34de86555e755dc05a14da
yevheniihudzinskyi <gudzinskyi@gmail.com>
2024-10-11 17:06:07 +0300

    tasks

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,10 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::post('/server', ProvisionServer::class);
-
 Route::resource('tasks', TaskController::class);
 
-Route::get('tasks/index', [TaskController::class, 'index'])
-    ->name('tasks.index');
+//Route::get('tasks/index', [TaskController::class, 'index'])
+//    ->name('tasks.index');
 //
 //Route::get('tasks/create', [TaskController::class, 'create'])
 //    ->name('tasks.create');
